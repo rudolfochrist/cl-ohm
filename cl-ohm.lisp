7853114d650e66926a50ac8aa5ccc6ab4d4729f5
Sebastian Christ <rudolfo.christ@gmail.com>
2016-01-19 13:52:47 +0100

    Creating, saving, deleting of simple models.
    Models with just some attributes.

--- cl-ohm.lisp
+++ cl-ohm.lisp
@@ -23,6 +23,12 @@
                                           connection-details)
      ,@body))
 
+(defmacro with-transactional-connection (connection-details &body body)
+  `(with-connection ,connection-details
+     (red:multi)
+     ,@body
+     (red:exec)))
+
 (define-condition ohm-error (error)
   ())
 
@@ -40,9 +46,9 @@
 (define-condition ohm-missig-id-error (ohm-error)
   ((unmanaged-object :initarg :object
                      :accessor unmanaged-object))
-  (:report (lambda (condtion stream))
-           (format stream "The object ~A is missing a ID and is therefore unmanaged. Please create~
-managed objects with the function CREATE." (unmanaged-object condition))))
+  (:report (lambda (condition stream)
+             (format stream "The object ~A is missing an ID and is therefore unmanaged. Please create~
+managed objects with the function CREATE." (unmanaged-object condition)))))
 
 (define-constant +global-id-counter+ "CL-OHM-GLOBAL-ID-COUNTER" :test #'string=)
 
@@ -59,22 +65,65 @@ managed objects with the function CREATE." (unmanaged-object condition))))
   (:method ((model ohm-model))
     (slot-boundp model 'id)))
 
-(defgeneric key (model &rest segments)
-  (:documentation "Ohm Model key accessor.")
+(defgeneric object-key (model &rest segments)
+  (:documentation "Generates an Ohm model object key.")
+  (:method :before ((model ohm-model) &rest segments)
+           (declare (ignore segments))
+           (unless (managed-object-p model)
+             (error 'ohm-missig-id-error :object model)))
   (:method ((model ohm-model) &rest segments)
-    (when (persisted-p model)
+    (with-output-to-string (out)
+      (format out "~A:~A" (class-name (class-of model)) (id model))
+      (when (consp segments)
+        (format out "~{:~A~}" segments)))))
 
-      (with-output-to-string (out)
-        (format out "~A:~A" (class-name (class-of model)) (id model))
-        (when (consp segments)
-          (format out "~{:~A~}" segments))))))
+(defgeneric class-key (model &rest segments)
+  (:documentation "Generates a Ohm Model Class Key.")
+  (:method :before ((model ohm-model) &rest segments)
+           (declare (ignore segments))
+           (unless (managed-object-p model)
+             (error 'ohm-missig-id-error :object model)))
+  (:method ((model ohm-model) &rest segments)
+    (with-output-to-string (out)
+      (format out "~A" (class-name (class-of model)))
+      (when (consp segments)
+        (format out "~{:~A~}" segments)))))
 
 (defun create (model-class &rest initargs)
   "Creates a new managed object."
-  (assert (subtypep model-class 'ohm-model) nil
-          "Cannot created an instance of ~A. Please use DEFOHM to define a suitable persistence type" model-class)
+  (assert (subtypep model-class 'ohm-model)
+          (model-class)
+          "Cannot create a persisted instance of ~A. Please use DEFOHM to define a suitable persistence type."
+          model-class)
   (let ((instance (apply #'make-instance model-class initargs)))
     (with-connection ()
       (let ((id (red:incr +global-id-counter+)))
         (setf (slot-value instance 'id) id)
         instance))))
+
+(defun slots->plist (object)
+  (loop for slot in (closer-mop:class-direct-slots (class-of object))
+     nconc (let ((slot-name (closer-mop:slot-definition-name slot)))
+             (when (slot-boundp object slot-name)
+               (list slot-name (slot-value object slot-name))))))
+
+(defgeneric save (model)
+  (:documentation "Saves the MODEL into the data store. Model must be a managed object defined with DEFOHM and created
+with CREATE.")
+  (:method :before ((model ohm-model))
+           (unless (managed-object-p model)
+             (error 'ohm-missig-id-error :object model)))
+  (:method ((model ohm-model))
+    (with-transactional-connection ()
+      (apply #'red:hmset (object-key model) (slots->plist model))
+      (red:sadd (class-key model 'all) (id model)))))
+
+(defgeneric del (model)
+  (:documentation "Removes MODEL from the data store.")
+  (:method :before ((model ohm-model))
+           (unless (managed-object-p model)
+             (error 'ohm-missig-id-error :object model)))
+  (:method ((model ohm-model))
+    (with-transactional-connection ()
+      (red:del (object-key model))
+      (red:srem (class-key model 'all) (id model)))))
