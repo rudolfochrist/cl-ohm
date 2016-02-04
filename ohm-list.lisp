0500264f843504c7593d1cf8f5b391d6e6d873c3
Sebastian Christ <rudolfo.christ@gmail.com>
2016-02-04 16:49:16 +0100

    DRYing up list access.

--- ohm-list.lisp
+++ ohm-list.lisp
@@ -40,10 +40,10 @@
     (with-connection ()
       (red:lrem (list-key list) 0 (ohm-id element)))))
 
-(defun generic-pop (list pop-func)
+(defun list-access (list pop-func &rest args)
   "Returns an object if pop-func delivers an id."
   (let ((id (with-connection ()
-              (funcall pop-func (list-key list)))))
+              (apply pop-func (list-key list) args))))
     (when id
       (plist->object (element-type list)
                      (fetch-one (element-type list) id)))))
@@ -51,21 +51,17 @@
 (defgeneric list-pop (list)
   (:documentation "Removes and returns an the right outermost element from the LIST.")
   (:method ((list ohm-list))
-    (generic-pop list #'red:rpop)))
+    (list-access list #'red:rpop)))
 
 (defgeneric list-pop-left (list)
   (:documentation "Removes and returns the left outermost element of the LIST.")
   (:method ((list ohm-list))
-    (generic-pop list #'red:lpop)))
+    (list-access list #'red:lpop)))
 
 (defgeneric list-index (list index)
   (:documentation "Returns the element with INDEX from LIST.")
   (:method ((list ohm-list) (index integer))
-    (let ((id (with-connection ()
-                (red:lindex (list-key list) index))))
-      (when id
-        (plist->object (element-type list)
-                       (fetch-one (element-type list) id))))))
+    (list-access list #'red:lindex index)))
 
 (defgeneric list-first (list)
   (:documentation "Returns the first element of the LIST.")
@@ -77,16 +73,21 @@
   (:method ((list ohm-list))
     (list-index list -1)))
 
+(defgeneric list-ids (list &optional start stop)
+  (:documentation "Returns the IDs stored in LIST.")
+  (:method ((list ohm-list) &optional (start 0) (stop -1))
+    (with-connection ()
+      (red:lrange (list-key list) start stop))))
+
 (defgeneric list-member (list element)
   (:documentation "Checks if ELEMENT is a member of LIST.")
   (:method :before ((list ohm-list) (element ohm-object))
            (declare (ignore list))
            (ensure-id element))
   (:method ((list ohm-list) (element ohm-object))
-    (let ((ids (with-connection ()
-                 (red:lrange (list-key list) 0 -1)))
-          (element-id (ohm-id element)))
-      (member element-id ids :test #'string=))))
+    (member (ohm-id element)
+            (list-ids list)
+            :test #'string=)))
 
 (defgeneric list-range (list start stop)
   (:documentation "Returns a sublist of LIST from START to STOP.")
@@ -94,8 +95,7 @@
     (mapcar (lambda (plist)
               (plist->object (element-type list) plist))
             (fetch (element-type list)
-                   (with-connection ()
-                     (red:lrange (list-key list) start stop))))))
+                   (list-ids list)))))
 
 (defgeneric list-replace (list new-elements)
   (:documentation "Replaces the LIST elements with NEW-ELEMENTS.")
@@ -109,3 +109,8 @@
           (red:del (list-key list))
           (dolist (id ids)
             (red:rpush (list-key list) id)))))))
+
+(defgeneric list-elements (list)
+  (:documentation "Returns all elements in LIST.")
+  (:method ((list ohm-list))
+    (list-range list 0 -1)))
